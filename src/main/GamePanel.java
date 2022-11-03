67f52f8e1ff27afb6bd73aeec3cbe2795cba4497
Shamsuddin Refaei <shamss.aldin@gmail.com>
2022-11-03 15:41:24 +0000

    -Add getRandColor method to create & return a random color -Change color of rectangle every time it changes direction

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -5,6 +5,7 @@ import input.MouseInput;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
 
 // Game panel class allows drawing and painting inside game frame
 public class GamePanel extends JPanel {
@@ -12,15 +13,20 @@ public class GamePanel extends JPanel {
     // Variables that increase or decrease rectangle position depending on user input
     private float xDelta = 100;
     private float yDelta = 100;
-    private float xDir = 0.003f;
-    private float yDir = 0.003f;
+    private float xDir = 0.01f;
+    private float yDir = 0.01f;
 
+    // Variables that help display frames per second in console
     private int frames = 0;
     private long lastCheck = 0;
+
+    private Color color = new Color(194, 34, 123);
+    private Random random;
 
     // Constructor where input is taken care of
     public GamePanel() {
 
+        random = new Random();
         MouseInput mouseInput = new MouseInput(this);
         addKeyListener(new KeyboardInput(this));
         addMouseListener(mouseInput);
@@ -52,6 +58,7 @@ public class GamePanel extends JPanel {
         super.paintComponent(g);
 
         updateRectangle();
+        g.setColor(color);
         g.fillRect((int)xDelta, (int)yDelta, 200, 50);
 
         // Create FPS counter in console
@@ -66,16 +73,29 @@ public class GamePanel extends JPanel {
     }
 
     // Method to update rectangle position and make it move inside boundaries of game window
+    // Update rectangle color everytime it changes direction
     private void updateRectangle() {
 
         xDelta += xDir;
         if(xDelta > 400 || xDelta < 0) {
             xDir *= -1;
+            color = getRandColor();
         }
 
         yDelta += yDir;
         if(yDelta > 400 || yDelta < 0) {
             yDir *= -1;
+            color = getRandColor();
         }
+    }
+
+    // Method to create and return a random color
+    private Color getRandColor() {
+
+        int r = random.nextInt(255);
+        int g = random.nextInt(255);;
+        int b = random.nextInt(255);;
+
+        return new Color(r, g, b);
     }
 }
