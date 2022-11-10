cf7e739579aecfa9ef382f128cea45bdb73dbf94
Shamsuddin Refaei <shamss.aldin@gmail.com>
2022-11-10 13:35:53 +0000

    Fixed running animation issue when scale is 1.5f -Fixed player speed issue & changed it from constant to relative to game scale

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -26,7 +26,7 @@ public class Player extends Entity{
     private int playerAction = IDLE;
     private boolean moving = false, attacking = false;
     private boolean left, up, right, down, jump;
-    private float playerSpeed = 2.0f;
+    private float playerSpeed = 1.0f * Game.SCALE;
     private int[][] levelData;  // Store level data in Player class to allow collision detection
 
     // Offset for accurate player hitbox compared to original hitbox
@@ -43,7 +43,7 @@ public class Player extends Entity{
     public Player(float x, float y, int width, int height) {
         super(x, y, width, height);
         loadAnimations();
-        initHitbox(x, y, 20 * Game.SCALE, 28 * Game.SCALE);
+        initHitbox(x, y, (int) (20 * Game.SCALE), (int) (27 * Game.SCALE));
     }
 
     // Method to implement logic for game and update animations
