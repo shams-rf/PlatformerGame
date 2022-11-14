03ad10bbf247609980ea2860346438cd98b62b3d
Shamsuddin Refaei <shamss.aldin@gmail.com>
2022-11-14 18:38:27 +0000

    Move update & updateMove methods from enemy class into Crabby class

--- src/entities/Crabby.java
+++ src/entities/Crabby.java
@@ -2,7 +2,9 @@ package entities;
 
 import main.Game;
 
+import static utilities.Constants.Directions.LEFT;
 import static utilities.Constants.EnemyConstants.*;
+import static utilities.HelpMethods.*;
 
 // Crabby class that extends & inherits from Enemy class
 public class Crabby extends Enemy {
@@ -10,5 +12,74 @@ public class Crabby extends Enemy {
     public Crabby(float x, float y) {
         super(x, y, CRABBY_WIDTH, CRABBY_HEIGHT, CRABBY);
         initHitbox(x, y, (int) (22 * Game.SCALE), (int) (19 * Game.SCALE));
+    }
+
+    public void update(int[][] levelData) {
+
+        updateMove(levelData);
+        updateAnimationTick();
+    }
+
+    // Method to update movement of enemy
+    // If enemy just spawned (first update), then make them fall to the ground from the air
+    // Otherwise, enemy patrols in a set pattern until it sees a player
+    private void updateMove(int[][] levelData) {
+
+        if(firstUpdate) {
+
+            if(!isEntityOnFloor(hitbox, levelData)) {
+
+                inAir = true;
+            }
+
+            firstUpdate = false;
+        }
+
+        if(inAir) {
+
+            if(canMoveHere(hitbox.x, hitbox.y + fallSpeed, hitbox.width, hitbox.height, levelData)) {
+
+                hitbox.y += fallSpeed;
+                fallSpeed += gravity;
+            }
+            else {
+
+                inAir = false;
+                hitbox.y = getEntityYPosUnderRoofOrAboveFloor(hitbox, fallSpeed);
+            }
+        }
+        else {
+
+            switch(enemyState) {
+
+                case IDLE:
+                    enemyState = RUNNING;
+                    break;
+                case RUNNING:
+                    float xSpeed = 0;
+
+                    if(walkDir == LEFT) {
+
+                        xSpeed = -walkSpeed;
+                    }
+                    else {
+
+                        xSpeed = walkSpeed;
+                    }
+
+                    if(canMoveHere(hitbox.x + xSpeed, hitbox.y, hitbox.width, hitbox.height, levelData)) {
+
+                        if(isFloor(hitbox, xSpeed, levelData)) {
+
+                            hitbox.x += xSpeed;
+                            return;
+                        }
+                    }
+
+                    changeWalkDir();
+
+                    break;
+            }
+        }
     }
 }
--- src/entities/Enemy.java
+++ src/entities/Enemy.java
@@ -10,20 +10,20 @@ import static utilities.HelpMethods.*;
 // Abstract class means objects cannot be created from this class.
 public abstract class Enemy extends Entity {
 
-    private int enemyState; // Integer to specify if enemy is running, idle, attacking
-    private int enemyType;
+    protected int enemyState; // Integer to specify if enemy is running, idle, attacking
+    protected int enemyType;
 
     // Variables to store animation properties
-    private int animIndex, animTick;
-    private int animSpeed = 25;
+    protected int animIndex, animTick;
+    protected int animSpeed = 25;
 
     // Variables that handle enemy movement & falling
-    private boolean firstUpdate = true; // If it's the enemy's first update, make them fall to the ground from the air
-    private boolean inAir;
-    private float fallSpeed;
-    private float gravity = 0.04f * Game.SCALE;
-    private float walkSpeed = 0.5f * Game.SCALE;
-    private int walkDir = LEFT;
+    protected boolean firstUpdate = true; // If it's the enemy's first update, make them fall to the ground from the air
+    protected boolean inAir;
+    protected float fallSpeed;
+    protected float gravity = 0.04f * Game.SCALE;
+    protected float walkSpeed = 0.5f * Game.SCALE;
+    protected int walkDir = LEFT;
 
     public Enemy(float x, float y, int width, int height, int enemyType) {
         super(x, y, width, height);
@@ -31,7 +31,7 @@ public abstract class Enemy extends Entity {
         initHitbox(x, y, width, height);
     }
 
-    private void updateAnimationTick() {
+    protected void updateAnimationTick() {
 
         animTick++;
 
@@ -47,77 +47,8 @@ public abstract class Enemy extends Entity {
         }
     }
 
-    public void update(int[][] levelData) {
-
-        updateMove(levelData);
-        updateAnimationTick();
-    }
-
-    // Method to update movement of enemy
-    // If enemy just spawned (first update), then make them fall to the ground from the air
-    // Otherwise, enemy patrols in a set pattern until it sees a player
-    private void updateMove(int[][] levelData) {
-
-        if(firstUpdate) {
-
-            if(!isEntityOnFloor(hitbox, levelData)) {
-
-                inAir = true;
-            }
-
-            firstUpdate = false;
-        }
-
-        if(inAir) {
-
-            if(canMoveHere(hitbox.x, hitbox.y + fallSpeed, hitbox.width, hitbox.height, levelData)) {
-
-                hitbox.y += fallSpeed;
-                fallSpeed += gravity;
-            }
-            else {
-
-                inAir = false;
-                hitbox.y = getEntityYPosUnderRoofOrAboveFloor(hitbox, fallSpeed);
-            }
-        }
-        else {
-
-            switch(enemyState) {
-
-                case IDLE:
-                    enemyState = RUNNING;
-                    break;
-                case RUNNING:
-                    float xSpeed = 0;
-
-                    if(walkDir == LEFT) {
-
-                        xSpeed = -walkSpeed;
-                    }
-                    else {
-
-                        xSpeed = walkSpeed;
-                    }
-
-                    if(canMoveHere(hitbox.x + xSpeed, hitbox.y, hitbox.width, hitbox.height, levelData)) {
-
-                        if(isFloor(hitbox, xSpeed, levelData)) {
-
-                            hitbox.x += xSpeed;
-                            return;
-                        }
-                    }
-
-                    changeWalkDir();
-
-                    break;
-            }
-        }
-    }
-
     // Method to change enemy walking direction from right to left & vice versa
-    private void changeWalkDir() {
+    protected void changeWalkDir() {
 
         if(walkDir == LEFT) {
 
